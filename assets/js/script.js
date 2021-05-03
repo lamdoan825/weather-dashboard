3e6e1f67a9cfadf1eb6548d08268ffe658323130
lamdoan825@gmail.com <lam@Lams-Air.attlocal.net>
2021-05-02 20:42:33 -0500

    pseudo coded

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function begin() {
+	//naming function
+
+
+	// Assigning API to a variable
+
+
+	// Execute a current weather get request from open weather api
+
+					
+	// Get UV Index
+
+
+	// Get 5 day forecast for this city
+
+
+	// Icon for current weather
+
+
+	// history from local storage
+
+
+	// Clear History button
+
+}
+
+begin();
